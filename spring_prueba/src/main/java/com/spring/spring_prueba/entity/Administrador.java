060f4763ca464322000595124a9822e456ff3ef0
José Alfredo López De La Cruz <josealfredolopezdelacruz2@gmail.com>
2025-07-03 03:15:44 +0000

    AQUI YA FUNCIONO TOMA EN CUENTA LA CONFIGURACION DE properties

--- spring_prueba/src/main/java/com/spring/spring_prueba/entity/Administrador.java
+++ spring_prueba/src/main/java/com/spring/spring_prueba/entity/Administrador.java
@@ -1,10 +1,21 @@
 package com.spring.spring_prueba.entity;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 public class Administrador {
 
-    @id
+    @Id
     private long id;
+    @Column(nullable = false)
     private String correo;
+    @Column(nullable = false)
     private String contraseña;
 }
